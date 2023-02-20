aa8064f4d743f7783aac4429538c4daa77255360
Johnnycus <Johnnycus@users.noreply.github.com>
2023-02-20 23:00:41 +0600

    Add ability to save customized character sprites

--- characterCustomizer.js
+++ characterCustomizer.js
@@ -1,5 +1,121 @@
-function saveCharacter(selectedItems) {
-  alert(JSON.stringify(selectedItems, null, 2))
+// poseName: numberOfPoseFrames (starting from 0)
+const poses = {
+  slashingBehind: 3,
+  alert: 2,
+  bashing: 3,
+  firingBigBow: 3,
+  firingBow: 3,
+  firingCrossbow: 4,
+  flying: 2,
+  jumping: 1,
+  jumpingSmash: 4,
+  lungingOneHanded: 3,
+  lungingTwoHanded: 4,
+  lyingDown: 1,
+  lyingDownStabbing: 2,
+  sitting: 1,
+  slashingFront: 3,
+  slashingUpward: 3,
+  smashing: 3,
+  spinningSlash: 4,
+  spinningThrow: 4,
+  stabbingOneHanded: 2,
+  stabbingTwoHanded: 3,
+  standingOneHanded: 2,
+  standingTwoHanded: 2,
+  throwingBackhanded: 3,
+  throwingDownward: 3,
+  throwingForehanded: 3,
+  thrustingOneHanded: 2,
+  thrustingTwoHanded: 3,
+  walkingOneHanded: 4,
+  walkingTwoHanded: 4,
+}
+
+function readBlob(b) {
+  return new Promise(function (resolve, reject) {
+    const reader = new FileReader()
+
+    reader.onloadend = function () {
+      resolve(reader.result)
+    }
+
+    reader.readAsDataURL(b)
+  })
+}
+
+async function characterPreviewFetcher(selectors, zip, pose, poseFrame) {
+  const res = await fetch('https://api.maplestory.net/character/render', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      skin: selectors.skin,
+      ears: selectors.ears,
+      faceEmote: selectors.expression,
+      hairId: selectors.hair.hairId,
+      faceId: selectors.face.faceId,
+      itemIds: [
+        selectors.faceAccessory && selectors.faceAccessory.itemId,
+        selectors.eyeAccessory && selectors.eyeAccessory.itemId,
+        selectors.hat && selectors.hat.itemId,
+        selectors.earrings && selectors.earrings.itemId,
+        selectors.top && selectors.top.itemId,
+        selectors.bottom && selectors.bottom.itemId,
+        selectors.overall && selectors.overall.itemId,
+        selectors.gloves && selectors.gloves.itemId,
+        selectors.weapon && selectors.weapon.itemId,
+        selectors.shield && selectors.shield.itemId,
+        selectors.cape && selectors.cape.itemId,
+        selectors.shoes && selectors.shoes.itemId,
+      ].filter(Boolean),
+      pose,
+      poseFrame,
+    }),
+  })
+
+  const blob = await res.blob()
+
+  // Don't do anything if the image is broken (due to an invalid pose)
+  if (blob === undefined) {
+    return
+  }
+
+  // If zip is specified - save img there
+  if (zip) {
+    zip.file(`${pose}-${poseFrame}.png`, blob)
+  } else {
+    // Otherwise just return the blob
+    return blob
+  }
+}
+
+const generateSpritesheet = async (zip, selectors) => {
+  const spritesheetFolder = zip.folder('spritesheet')
+  const posesToFetch = []
+
+  for (const [pose, numOfFrames] of Object.entries(poses)) {
+    Array.from({ length: numOfFrames }, (_, i) => {
+      posesToFetch.push(
+        characterPreviewFetcher(selectors, spritesheetFolder, pose, i),
+      )
+    })
+  }
+
+  return Promise.all(posesToFetch)
+}
+
+const saveZip = async (zip) => {
+  const blob = await zip.generateAsync({ type: 'blob' })
+  saveAs(blob, 'spritesheet.zip')
+}
+
+async function characterSaver(selectors) {
+  const zip = new JSZip()
+  await generateSpritesheet(zip, selectors)
+  saveZip(zip)
 }
 
 const characterOptions = {
@@ -171,18 +287,6 @@ const characterOptions = {
   ],
 }
 
-function readBlob(b) {
-  return new Promise(function (resolve, reject) {
-    const reader = new FileReader()
-
-    reader.onloadend = function () {
-      resolve(reader.result)
-    }
-
-    reader.readAsDataURL(b)
-  })
-}
-
 const defaultSelectors = {
   skin: characterOptions.skins[0].value,
   expression: undefined,
@@ -215,39 +319,7 @@ const menuData = {
   selectors: defaultSelectors,
   characterPreview: null,
   async fetchCharacterPreview() {
-    const rawResponse = await fetch(
-      'https://api.maplestory.net/character/render',
-      {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          skin: this.selectors.skin,
-          ears: this.selectors.ears,
-          faceEmote: this.selectors.expression,
-          hairId: this.selectors.hair.hairId,
-          faceId: this.selectors.face.faceId,
-          itemIds: [
-            this.selectors.faceAccessory && this.selectors.faceAccessory.itemId,
-            this.selectors.eyeAccessory && this.selectors.eyeAccessory.itemId,
-            this.selectors.hat && this.selectors.hat.itemId,
-            this.selectors.earrings && this.selectors.earrings.itemId,
-            this.selectors.top && this.selectors.top.itemId,
-            this.selectors.bottom && this.selectors.bottom.itemId,
-            this.selectors.overall && this.selectors.overall.itemId,
-            this.selectors.gloves && this.selectors.gloves.itemId,
-            this.selectors.weapon && this.selectors.weapon.itemId,
-            this.selectors.shield && this.selectors.shield.itemId,
-            this.selectors.cape && this.selectors.cape.itemId,
-            this.selectors.shoes && this.selectors.shoes.itemId,
-          ].filter(Boolean),
-        }),
-      },
-    )
-
-    const blob = await rawResponse.blob()
+    const blob = await characterPreviewFetcher(this.selectors)
 
     const characterPreviewURI = await readBlob(blob)
 
