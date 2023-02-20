55adf11ab919f1167e93eee19956de83718c5f37
billythedummy <dhy1996@live.com.sg>
2023-02-20 17:25:20 +0800

    refactor to reduce number of callbacks

--- minimalTest.js
+++ minimalTest.js
@@ -36,20 +36,19 @@ function setRoomList() {
         // < 0 = a comes first (lower index) - we want high indexes = newer
         var aMsg = a.timeline[a.timeline.length - 1];
         if (!aMsg) {
-            return -1;
+            return 1;
         }
         var bMsg = b.timeline[b.timeline.length - 1];
         if (!bMsg) {
-            return 1;
+            return -1;
         }
         if (aMsg.getTs() > bMsg.getTs()) {
-            return 1;
-        } else if (aMsg.getTs() < bMsg.getTs()) {
             return -1;
+        } else if (aMsg.getTs() < bMsg.getTs()) {
+            return 1;
         }
         return 0;
     });
-    roomList = roomList.reverse()
 }
 
 function setActiveRoom(roomId) {
@@ -83,41 +82,57 @@ function isRecentEvent(event) {
     return Math.abs(event.localTimestamp - Date.now()) < 60_000;
 }
 
-async function start() {
-    await client.startClient();
-    
-    client.once('sync', function(state, prevState, res) {
-        console.log(state); // state will be 'PREPARED' when the client is ready to use
-        switch (state) {
-            case "PREPARED":
-                setRoomList();
-                render();
-                break;
-        }
-    });
-
+function setCallbacksOnPrepared() {
     client.on("Room", () => {
         setRoomList();
-    })
+        render();
+    });
 
     client.on("Room.timeline", (event, room, toStartOfTimeline) => {
-        if (toStartOfTimeline) {
-            return; // don't print paginated results
-        }
-        if (event.getType() !== "m.room.message") {
-            return; // only print messages
-        }
-
-        const c = event.getContent();
-        messageHistory[room.roomId] += `${event.getSender()}: ${c.body} <br/>`;
-
+        appendMessageEvent(event, room, toStartOfTimeline);
         // only send recent/new messages to game
         if (gameCommOnMatrixMsg && isRecentEvent(event)) {
             gameCommOnMatrixMsg(event);
         }
+        // so that most recent appears first
         setRoomList();
         render();
     });
+}
+
+function appendMessageEvent(event, room, toStartOfTimeline) {
+    if (toStartOfTimeline) {
+        return; // don't print paginated results
+    }
+    if (event.getType() !== "m.room.message") {
+        return; // only print messages
+    }
+
+    const c = event.getContent();
+    messageHistory[room.roomId] += `${event.getSender()}: ${c.body} <br/>`;
+}
+
+async function start() { 
+    client.on("Room.timeline", appendMessageEvent);
+    
+    // See matrix-js-sdk src/client.ts comment:
+    // initial state can only transition to either "PREPARED" or "ERROR",
+    // so once() is safe
+    client.once("sync", function (state, _prevState, _res) {
+        console.log(state); // state will be 'PREPARED' when the client is ready to use
+        switch (state) {
+            case "PREPARED":
+                client.off("Room.timeline", appendMessageEvent);
+                setCallbacksOnPrepared();
+                setRoomList();
+                render();
+                break;
+            case "ERROR":
+                throw new Error("Error syncing matrix");
+        }
+    });
+
+    await client.startClient();
 }
 
 function logout() {
