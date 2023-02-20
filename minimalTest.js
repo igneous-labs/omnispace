6e187d6b21aeee4d5ec3fe1e8506230e54d4ce46
fp <fp@socean.fi>
2023-02-20 17:29:13 +0800

    address HYs nits

--- minimalTest.js
+++ minimalTest.js
@@ -36,20 +36,22 @@ function setRoomList() {
         // < 0 = a comes first (lower index) - we want high indexes = newer
         var aMsg = a.timeline[a.timeline.length - 1];
         if (!aMsg) {
-            return -1;
+            return 1;
         }
         var bMsg = b.timeline[b.timeline.length - 1];
         if (!bMsg) {
-            return 1;
+            return -1;
         }
         if (aMsg.getTs() > bMsg.getTs()) {
-            return 1;
-        } else if (aMsg.getTs() < bMsg.getTs()) {
             return -1;
+        } else if (aMsg.getTs() < bMsg.getTs()) {
+            return 1;
         }
         return 0;
     });
-    tmp = tmp.reverse()
+ 
+    // making use of Map's property that it respects insertion order: that's
+    // why we create a new Map rather than just editing the old one.
     let tmp2 = new Map();
     tmp.map((room) => {tmp2.set(room.roomId, room)})
     roomList = tmp2
