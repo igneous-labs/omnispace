a8f4660630a5a62e5be4d11281caf7a25a53b710
Johnnycus <Johnnycus@users.noreply.github.com>
2023-02-27 14:11:13 +0600

    Absolute canvas and sticky header

--- minimalTest.js
+++ minimalTest.js
@@ -351,41 +351,49 @@ async function logout() {
 
 document.addEventListener('paste', handlePaste);
 
-let pendingUpdate = false;
-const viewportHandler = (event) => {
-    if (pendingUpdate) {
-        return;
-    }
-    pendingUpdate = true;
-
-    requestAnimationFrame(() => {
-        pendingUpdate = false;
-        const layoutViewport = document.getElementById('game');
-        layoutViewport.style.transform = "none";
-        if (layoutViewport.getBoundingClientRect().top < 0) {
-            layoutViewport.style.transform = `translate(0, ${-layoutViewport.getBoundingClientRect().top}px)`;
-        }
-    });
-};
-window.visualViewport.addEventListener("scroll", viewportHandler);
-window.visualViewport.addEventListener("resize", viewportHandler);
-
-// const MIN_KEYBOARD_HEIGHT = 300;
-// let canScroll = true;
-// window.visualViewport.addEventListener("resize", (event) => {
-//     if (!isMobile) return;
-//     var chat_area = document.getElementById("chat_area");
-//     var header = document.getElementById("header");
-//     if (window.screen.height - MIN_KEYBOARD_HEIGHT > window.visualViewport.height) {
-//       chat_area.classList.add('keyboard_open');
-//       window.scrollTo(0, header?.clientHeight + 26)
-//       canScroll = false;
-//     } else {
-//       chat_area.classList.remove('keyboard_open');
-//       window.scrollTo(0, document.body.scrollHeight)
-//       canScroll = true;
+// let pendingUpdate = false;
+// const viewportHandler = (event) => {
+//     if (pendingUpdate) {
+//         return;
 //     }
-// });
+//     pendingUpdate = true;
+
+//     requestAnimationFrame(() => {
+//         pendingUpdate = false;
+//         const layoutViewport = document.getElementById('game');
+//         layoutViewport.style.transform = "none";
+//         document.body.style.overflow = "";
+//         if (layoutViewport.getBoundingClientRect().top < 0) {
+//             layoutViewport.style.transform = `translate(0, ${-layoutViewport.getBoundingClientRect().top}px)`;
+//             document.body.style.overflow = "hidden";
+//         }
+//     });
+// };
+// window.visualViewport.addEventListener("scroll", viewportHandler);
+// window.visualViewport.addEventListener("resize", viewportHandler);
+
+const MIN_KEYBOARD_HEIGHT = 300;
+// let canScroll = true;
+window.visualViewport.addEventListener("resize", (event) => {
+    if (!isMobile) return;
+    var chat_area = document.getElementById("chat_area");
+    var view = document.getElementById("view");
+    var header = document.getElementById("header");
+    if (window.screen.height - MIN_KEYBOARD_HEIGHT > window.visualViewport.height) {
+      chat_area.style.marginTop = "0px";
+      view.style.maxHeight = "calc(400px - 48px)";
+      // alert(chat_area.style)
+      // chat_area.classList.add('keyboard_open');
+      window.scrollTo(0, header?.clientHeight + 26)
+      // canScroll = false;
+    } else {
+      chat_area.style.marginTop = "400px";
+      view.style.maxHeight = "calc(100vh - 48px - 100vw - 80px)";
+      // chat_area.classList.remove('keyboard_open');
+      window.scrollTo(0, document.body.scrollHeight)
+      // canScroll = true;
+    }
+});
 
 // document.body.addEventListener("touchmove", (event) => {
 //     if (isMobile && window.screen.height - MIN_KEYBOARD_HEIGHT > window.visualViewport.height && !canScroll) {
