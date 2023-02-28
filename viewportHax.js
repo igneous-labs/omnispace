a81b74f6bc080093f07c4a4b466fa02027f4f3a6
fp <fp@socean.fi>
2023-02-28 21:13:47 +0800

    set view height according to visual viewport

--- viewportHax.js
+++ viewportHax.js
@@ -17,15 +17,15 @@
 //   setViewHeight();
 // }
 
-// function setViewHeight() {
-//   const navHeight = document.querySelector("nav").offsetHeight;
-//   const footerHeight = document.querySelector("footer").offsetHeight;
-//   const vpH = window.visualViewport.height;
-//   // leave 15px for bottom margin between textrow and bottom of screen
-//   document.getElementById("view").style.height = `${Math.round(
-//     vpH - navHeight - footerHeight - 15,
-//   )}px`;
-// }
+function setViewHeight() {
+  const navHeight = document.querySelector("nav").offsetHeight;
+  const footerHeight = document.querySelector("footer").offsetHeight;
+  const vpH = window.visualViewport.height;
+  // leave 15px for bottom margin between textrow and bottom of screen
+  document.getElementById("view").style.height = `${Math.round(
+    vpH - navHeight - footerHeight - 15,
+  )}px`;
+}
 
 // function onVirtualKeyboardUp() {
 //   onVirtualKeyboard(true);
@@ -54,12 +54,12 @@
 //   // prevVVheight = vp.height;
 // }
 
-// window.visualViewport.addEventListener("resize", viewportHandler);
-// // only run initial set if in portrait mode (on mobile)
-// if (window.innerHeight > window.innerWidth) {
-//   onVirtualKeyboard();
-// }
+window.visualViewport.addEventListener("resize", setViewHeight);
+// only run initial set if in portrait mode (on mobile)
+if (window.innerHeight > window.innerWidth) {
+  setViewHeight();
+}
 
-// const navFooterResizeObs = new ResizeObserver(setViewHeight);
-// navFooterResizeObs.observe(document.querySelector("nav"));
-// navFooterResizeObs.observe(document.querySelector("footer"));
+const navFooterResizeObs = new ResizeObserver(setViewHeight);
+navFooterResizeObs.observe(document.querySelector("nav"));
+navFooterResizeObs.observe(document.querySelector("footer"));
