10813a2e6c22e2203bef3dc4153cb192d29d428c
fp <fp@socean.fi>
2023-02-28 17:58:24 +0800

    fuck it all

--- viewportHax.js
+++ viewportHax.js
@@ -1,18 +1,18 @@
 // Make sure script is ran in defer so that window is defined
 
-let prevVVheight = window.visualViewport.height;
+// let prevVVheight = window.visualViewport.height;
 
 function onVirtualKeyboard(isUp) {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox
-  const canvas = document.getElementById("canvas");
-  canvas.style.zIndex = isUp ? -1 : 1;
-  const view = document.getElementById("view");
-  view.style.backgroundColor = isUp ? "rgba(0, 0, 0, 0.5)" : "transparent";
-  // NB: padding, margin doesnt work
-  view.style.borderTop = `${
-    isUp ? 0 : Math.round(canvas.offsetHeight)
-  }px solid transparent`;
+  // document.body.scrollTop = 0; // For Safari
+  // document.documentElement.scrollTop = 0; // For Chrome, Firefox
+  // const canvas = document.getElementById("canvas");
+  // canvas.style.zIndex = isUp ? -1 : 1;
+  // const view = document.getElementById("view");
+  // view.style.backgroundColor = isUp ? "rgba(0, 0, 0, 0.5)" : "transparent";
+  // // NB: padding, margin doesnt work
+  // view.style.borderTop = `${
+  //   isUp ? 0 : Math.round(canvas.offsetHeight)
+  // }px solid transparent`;
 
   setViewHeight();
 }
@@ -40,17 +40,17 @@ function onVirtualKeyboardDown() {
  * @param {*} event
  */
 function viewportHandler(event) {
-  const vp = event.target;
-  // only run if in portrait mode (on mobile)
-  if (window.innerHeight <= window.innerWidth) {
-    return;
-  }
-  if (prevVVheight > vp.height) {
-    onVirtualKeyboardUp();
-  } else {
-    onVirtualKeyboardDown();
-  }
-  prevVVheight = vp.height;
+  // const vp = event.target;
+  // // only run if in portrait mode (on mobile)
+  // if (window.innerHeight <= window.innerWidth) {
+  //   return;
+  // }
+  // if (prevVVheight > vp.height) {
+  //   onVirtualKeyboardUp();
+  // } else {
+  //   onVirtualKeyboardDown();
+  // }
+  // prevVVheight = vp.height;
 }
 
 window.visualViewport.addEventListener("resize", viewportHandler);
