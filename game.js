80e51c17bf6d1efb2c07d51706bb044038f1a44a
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2023-03-02 15:53:11 +0800

    fp2

--- game.js
+++ game.js
@@ -231,7 +231,7 @@ function parseSpritesheetJson(j) {
 
 let PlayerSpriteSheetMap = {
   "default": "char_default",
-  "@fp:melchior.info": "char_fp",
+  "@fp:melchior.info": "char_fp2",
   "@pixisu:melchior.info": "char_pixisu",
   "@chinkeeyong:melchior.info": "char_chinkeeyong",
   "@Boven:melchior.info": "char_boven",
@@ -382,6 +382,18 @@ let SpriteSheetFrameMap = {
       [562, 671],
     ],
   },
+  char_fp2: {
+    standing: [
+      [1, 110],
+      [112, 221],
+    ],
+    walking: [
+      [223, 335],
+      [337, 446],
+      [448, 560],
+      [562, 671],
+    ],
+  },
 };
 
 /*
@@ -431,6 +443,7 @@ Game.load = function () {
     Loader.loadImage("char_rczjian", "./img/char_rczjian.png"),
     Loader.loadImage("char_sf", "./img/char_sf.png"),
     Loader.loadImage("char_seulgi", "./img/char_seulgi.png"),
+    Loader.loadImage("char_fp2", "./img/char_fp2.png"),
   ];
 };
 
