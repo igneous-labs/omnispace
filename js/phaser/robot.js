466fb24ac0c3cc0199d017663e6da5ec74e77638
billythedummy <dhy1996@live.com.sg>
2023-03-14 17:14:31 +0800

    click to move

--- js/phaser/robot.js
+++ js/phaser/robot.js
@@ -1,11 +1,24 @@
 import { ExtendedObject3D } from "@enable3d/phaser-extension";
+import { Vector3 } from "three";
 
 const ASSET_PATH = "/assets/robot.glb";
 const SCALE = 1 / 3;
 
+const WALKING_ANIM_KEY = "Walking";
+const IDLE_ANIM_KEY = "Idle";
+
+const DIST_EPS = 1e-2;
+const RADIANS_EPS = 3e-2;
+const ANGULAR_V = 2;
+
 export class Robot {
+  static SELF_TO_CAMERA = new Vector3(12, 8, 12);
+
   /** @type {ExtendedObject3D} */
   body;
+
+  /** @type {?Vector3} */
+  destination;
 
   /**
    *
@@ -47,7 +60,6 @@ export class Robot {
     third.physics.add.existing(this.body, {
       shape: "capsule",
       ignoreScale: true,
-      //
       height,
       radius: height / 2,
       offset: { y: -height },
@@ -55,5 +67,87 @@ export class Robot {
     this.body.body.setLinearFactor(1, 1, 0);
     this.body.body.setAngularFactor(0, 0, 0);
     this.body.body.setFriction(0);
+
+    this.destination = null;
+  }
+
+  /**
+   *
+   * @param {import("three").Camera} camera
+   */
+  updateCamera(camera) {
+    camera.position.copy(this.cameraWorld());
+    camera.lookAt(this.body.position);
+  }
+
+  /**
+   * Calculates the world position to place the camera
+   * to enforce an isometric view centered on (looking at) this robot
+   * @returns {Vector3}
+   */
+  cameraWorld() {
+    const res = new Vector3();
+    return res.copy(this.body.position).add(Robot.SELF_TO_CAMERA);
+  }
+
+  /**
+   *
+   * @param {Vector3} posWorld
+   */
+  moveTo(posWorld) {
+    this.destination = posWorld;
+  }
+
+  update() {
+    if (this.destination) {
+      if (this.body.position.distanceTo(this.destination) < DIST_EPS) {
+        this.destination = null;
+        this.body.body.setVelocity(0, 0, 0);
+      } else {
+        this.pathFindStep();
+      }
+    }
+    this.updateAnim();
+  }
+
+  /**
+   * Note: under physics, you cannot change the position directly using
+   * rotate* setPosition* etc, you can only set velocity
+   */
+  pathFindStep() {
+    /** @type {Vector3} */
+    // @ts-ignore
+    const dest = this.destination;
+    const dirFlat = new Vector3();
+    dirFlat.copy(dest).sub(this.body.position);
+    dirFlat.setY(0);
+    dirFlat.normalize();
+
+    const facingFlat = new Vector3();
+    this.body.getWorldDirection(facingFlat);
+
+    const angleDeltaFlat = facingFlat.angleTo(dirFlat);
+    // console.log(angleDeltaFlat);
+    if (Math.abs(angleDeltaFlat) > RADIANS_EPS) {
+      const cross = new Vector3().copy(facingFlat).cross(dirFlat);
+
+      this.body.body.setVelocity(0, 0, 0);
+      const isClockwise = cross.y > 0;
+      this.body.body.setAngularVelocityY(isClockwise ? ANGULAR_V : -ANGULAR_V);
+    } else {
+      this.body.body.setAngularVelocityY(0);
+      this.body.body.setVelocity(dirFlat.x, dirFlat.y, dirFlat.z);
+    }
+  }
+
+  updateAnim() {
+    if (this.destination && this.body.anims.current !== WALKING_ANIM_KEY) {
+      this.body.anims.play(WALKING_ANIM_KEY);
+    } else if (
+      this.destination === null &&
+      this.body.anims.current !== IDLE_ANIM_KEY
+    ) {
+      this.body.anims.play(IDLE_ANIM_KEY);
+    }
   }
 }
--- js/phaser/scene.js
+++ js/phaser/scene.js
@@ -1,15 +1,25 @@
 import { Scene3D } from "@enable3d/phaser-extension";
 import { Robot } from "@/js/phaser/robot";
+import { Raycaster } from "three";
 
 export class MainScene extends Scene3D {
   /** @type {Robot} */
   robot;
 
+  /** @type {Raycaster} */
+  raycaster;
+
   constructor() {
     super({ key: "MainScene" });
+    // @ts-ignore
+    this.robot = undefined;
+    this.raycaster = new Raycaster();
   }
 
   init() {
+    // y - up
+    // x - southeast
+    // z - southwest
     this.accessThirdDimension({ gravity: { x: 0, y: -9.8, z: 0 } });
   }
 
@@ -23,7 +33,83 @@ export class MainScene extends Scene3D {
     this.robot.body.anims.play("Idle");
 
     // @ts-ignore
-    // this draws the collision shapes etc
-    this.third.physics.debug.enable();
+    // this draws the collision outlines etc
+    // this.third.physics.debug.enable();
+
+    const { canvas } = this.game;
+    const touchHandler = this.handleTouchOrClick.bind(this);
+    canvas.addEventListener("touchstart", touchHandler);
+    canvas.addEventListener("touchmove", touchHandler);
+    canvas.addEventListener("click", touchHandler);
+  }
+
+  update(_time, _delta) {
+    if (!this.robot) {
+      return;
+    }
+    this.robot.update();
+    this.robot.updateCamera(this.third.camera);
+  }
+
+  /**
+   *
+   * @param {PointerEvent | TouchEvent} e
+   */
+  handleTouchOrClick(e) {
+    /** @type {HTMLCanvasElement} */
+    // @ts-ignore
+    const canvas = e.target;
+    /** @type {number} */
+    let playerX;
+    /** @type {number} */
+    let playerY;
+    // @ts-ignore
+    if (e.touches) {
+      /** @type {TouchEvent} */
+      // @ts-ignore
+      const eTouch = e;
+      playerX = eTouch.touches[0].pageX - canvas.offsetLeft;
+      playerY = eTouch.touches[0].pageY - canvas.offsetTop;
+      console.log(`Touch:  x: ${playerX}, y: ${playerY}`);
+      // @ts-ignore
+    } else if (e.type === "click" && e.button === 0) {
+      /** @type {PointerEvent} */
+      // @ts-ignore
+      const eClick = e;
+      playerX = eClick.pageX - canvas.offsetLeft;
+      playerY = eClick.pageY - canvas.offsetTop;
+      console.log(`Click:  x: ${playerX}, y: ${playerY}`);
+    } else {
+      return;
+    }
+    // convert to NDC: y is reversed
+    // See: https://threejs.org/docs/#api/en/core/Raycaster
+    playerX = (playerX / canvas.clientWidth) * 2 - 1;
+    playerY = -(playerY / canvas.clientHeight) * 2 + 1;
+    const destWorldCoords = this.canvas2DToWorld3D(playerX, playerY);
+    if (destWorldCoords) {
+      this.robot.moveTo(destWorldCoords);
+    } else {
+      console.log("Clicked off the ground");
+    }
+  }
+
+  /**
+   *
+   * @param {number} x in NDC (-1, 1)
+   * @param {number} y in NDC (-1, 1)
+   * @returns {?import("three").Vector3}
+   */
+  canvas2DToWorld3D(x, y) {
+    this.raycaster.setFromCamera({ x, y }, this.third.camera);
+    // emptySpace is located ~500 units away
+    const [closest, emptySpace] = this.raycaster.intersectObjects(
+      this.third.scene.children,
+    );
+    // closest is emptySpace, discard this result
+    if (!emptySpace) {
+      return null;
+    }
+    return closest.point;
   }
 }
